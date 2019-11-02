3ae54fbc337472bdce96898a0e82e595c11f729a
Teddi Woodson <teddiwood97@ufl.edu>
2019-11-02 10:11:03 -0400

    Define our function

--- main.js
+++ main.js
@@ -3,7 +3,13 @@ const myBoxes = document.getElementsByClassName('block');
 
 //The Functionality
 function loopThrough() {
-  console.log(this.textContent.length)
+
+  const changeColor = () => this.classList.toggle('red');
+
+  for (let i = 0; i < this.textContent.length; i++) {
+
+  };
+
 };
 
 //The Event Listener
