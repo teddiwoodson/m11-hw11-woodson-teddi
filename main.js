8ebcef4b3de91441ed59d83094afcd9e70fd98e1
Teddi Woodson <teddiwood97@ufl.edu>
2019-11-02 09:59:23 -0400

    Define the event listener and add a function

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 const myBoxes = document.getElementsByClassName('block');
 
 //The Functionality
+function loopThrough() {
 
+};
 
 //The Event Listener
+for (let i = 0; i < myBoxes.length; i++) {
+  myBoxes[i].addEventListener('click', loopThrough);
+};
